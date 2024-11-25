e4b1aa77a61ee45fa1df55005c53fcea5ba6b9a5
HoDucLinh <hoduclinh080204@gmail.com>
2024-11-25 23:15:26 +0700

    database

--- bookstoremanagement/models.py
+++ bookstoremanagement/models.py
@@ -1,22 +1,26 @@
+from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 
 from bookstoremanagement import app ,db
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True ,autoincrement=True)
+    name = db.Column(db.String(100),nullable=False)
+    description = db.Column(db.String(100))
+    employees = db.relationship('Employee', backref='role', lazy=True)
+
+    def __str__(self):
+        return self.name
 
 class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True ,autoincrement=True)
     name = db.Column(db.String(100),nullable=False)
     phoneNumber = db.Column(db.String(50),nullable=False)
     address = db.Column(db.String(100))
-    role_ID = relationship('Role' ,backref='Employee', lazy=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Role(db.Model):
-    id = db.Column(db.Integer, primary_key=True ,autoincrement=True)
-    name = db.Column(db.String(100),nullable=False)
-    description = db.Column(db.String(100))
+    role_id = Column(Integer, ForeignKey('role.id'), nullable=False)
+    reports = db.relationship('Report', backref='employee', lazy=True)
+    stockinvoices = db.relationship('StockInvoice', backref='employee', lazy=True)
+    saleinvoices = db.relationship('SaleInvoice', backref='employee', lazy=True)
 
     def __str__(self):
         return self.name
@@ -25,7 +29,7 @@ class Report(db.Model):
     id = db.Column(db.Integer, primary_key=True ,autoincrement=True)
     reportDate = db.Column(db.Date)
     reportType = db.Column(db.String(100))
-    employee_ID = relationship(Employee, backref='Report', lazy=True)
+    employee_id = Column(Integer, ForeignKey(Employee.id), nullable=False)
 
     def __str__(self):
         return self.name
@@ -37,7 +41,7 @@ class StockInvoice(db.Model):
     totalAmount = db.Column(db.Float)
     totalQuantity = db.Column(db.Integer)
     supplierName = db.Column(db.String(100))
-    employee_ID = relationship(Employee, backref='StockInvoice', lazy=True)
+    employee_id = Column(Integer, ForeignKey(Employee.id), nullable=False)
 
     def __str__(self):
         return self.name
@@ -48,7 +52,7 @@ class Book(db.Model):
     price = db.Column(db.Float)
     publisherName = db.Column(db.String(100))
     description = db.Column(db.String(100))
-    category_ID = relationship('Category' ,backref='Book', lazy=True)
+    category_id = Column(Integer, ForeignKey('category.id'), nullable=False)
 
     def __str__(self):
         return self.name
@@ -57,7 +61,8 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True ,autoincrement=True)
     name = db.Column(db.String(100),nullable=False)
     totalBook = db.Column(db.Integer)
-    stock_ID = relationship('Stock' ,backref='Category', lazy=True)
+    stock_id = Column(Integer, ForeignKey('stock.id'), nullable=False)
+    books = relationship(Book, backref='category', lazy=True)
 
     def __str__(self):
         return self.name
@@ -67,6 +72,7 @@ class Stock(db.Model):
     name = db.Column(db.String(100),nullable=False)
     description = db.Column(db.String(100))
     totalCategory = db.Column(db.Integer)
+    categories = relationship('Category' ,backref='Stock', lazy=True)
 
     def __str__(self):
         return self.name
@@ -78,39 +84,49 @@ class Customer(db.Model):
     address = db.Column(db.String(100))
     paymentInfo = db.Column(db.String(100))
     email = db.Column(db.String(100))
+    cart_id = Column(Integer, ForeignKey('cart.id'), nullable=False)
+    saleinvoices = relationship('SaleInvoice', backref='customer', lazy=True)
 
     def __str__(self):
         return self.name
 
 class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True ,autoincrement=True)
-    customer_ID = relationship(Customer , backref='Cart', lazy=True)
+    customer_id = Column(Integer, ForeignKey(Customer.id), nullable=False)
 
     def __str__(self):
         return self.name
 
-class SaleInvoice(db.Model):
+class CartDetail(db.Model):
     id = db.Column(db.Integer, primary_key=True ,autoincrement=True)
-    paymentStatus = db.Column(db.String(100))
-    paymentMethod_ID = relationship('PaymentMethod' , backref='SaleInvoice', lazy=True)
-    employee_ID = relationship(Employee , backref='SaleInvoice', lazy=True)
-    customer_ID = relationship(Customer , backref='SaleInvoice', lazy=True)
+    book_id = Column(Integer, ForeignKey(Book.id), nullable=False)
+    customer_id = Column(Integer, ForeignKey(Customer.id), nullable=False)
+    quantity = db.Column(db.Integer , nullable=False)
 
-    def __str__(self):
-        return self.name
 
 class PaymentMethod(db.Model):
     id = db.Column(db.Integer, primary_key=True ,autoincrement=True)
     name = db.Column(db.String(100),nullable=False)
     description = db.Column(db.String(100))
+    saleinvoices = relationship('SaleInvoice', backref='paymentMethod', lazy=True)
+
+    def __str__(self):
+        return self.name
+class SaleInvoice(db.Model):
+    id = db.Column(db.Integer, primary_key=True ,autoincrement=True)
+    paymentStatus = db.Column(db.String(100))
+    paymentMethod_id = Column(Integer, ForeignKey(PaymentMethod.id), nullable=False)
+    employee_id = Column(Integer, ForeignKey(Employee.id), nullable=False)
+    customer_id = Column(Integer, ForeignKey(Customer.id), nullable=False)
 
     def __str__(self):
         return self.name
 
+
 class DetailInvoice(db.Model):
     id = db.Column(db.Integer, primary_key=True ,autoincrement=True)
-    book_ID = relationship(Book , backref='DetailInvoice', lazy=True)
-    saleInvoice_ID = relationship(SaleInvoice , backref='DetailInvoice')
+    book_id = Column(Integer, ForeignKey(Book.id), nullable=False)
+    saleInvoice_id = Column(Integer, ForeignKey(SaleInvoice.id), nullable=False)
     orderDate = db.Column(db.Date)
     totalAmount = db.Column(db.Float)
     quantity = db.Column(db.Integer)
