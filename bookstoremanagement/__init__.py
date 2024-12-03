6cd01141f1e1d797ba580ef53e3fa43311d8c2c4
Your Name <you@example.com>
2024-12-04 00:23:43 +0700

    Copy changes

--- bookstoremanagement/__init__.py
+++ bookstoremanagement/__init__.py
@@ -1,28 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
-
+from flask_login import LoginManager
+import cloudinary
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:123456@localhost/bookstoredb?charset=utf8mb4"
+app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:08022004@localhost/bookstoredb?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
+login = LoginManager(app)
 
-admin = Admin(app=app, name='BookStore Admin', template_mode='bootstrap3', url='/admin')
-
-# Import các model 
-from bookstoremanagement.models import User, Book, Category, Stock, Cart, CartDetail, SaleInvoice, DetailInvoice, Report, StockInvoice
-
-# Thêm các model vào admin interface
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Book, db.session))
-admin.add_view(ModelView(Category, db.session))
-admin.add_view(ModelView(Stock, db.session))
-admin.add_view(ModelView(Cart, db.session))
-admin.add_view(ModelView(CartDetail, db.session))
-admin.add_view(ModelView(SaleInvoice, db.session))
-admin.add_view(ModelView(DetailInvoice, db.session))
-admin.add_view(ModelView(Report, db.session))
-admin.add_view(ModelView(StockInvoice, db.session))
+cloudinary.config(cloud_name='dzwsdpjgi',
+                  api_key='693865187219449',
+                  api_secret='PtxvcgqYO2dZs7RDWJeNc2DA5Ew')
--- bookstoremanagement/models.py
+++ bookstoremanagement/models.py
@@ -1,4 +1,6 @@
-from email.policy import default
+import hashlib
+from datetime import date
+
 from flask import json
 from sqlalchemy import Column, Integer, ForeignKey, String
 from sqlalchemy.orm import relationship
@@ -8,14 +10,13 @@ from bookstoremanagement import app ,db
 from flask_login import UserMixin
 
 
-
 class UserRole(PyEnum):
     ADMIN = 1
     SALE = 2
     USER = 3
 
 
-class User(db.Model, UserMixin):
+class User(db.Model , UserMixin):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     username = db.Column(db.String(100), nullable=False, unique=True)
@@ -113,6 +114,7 @@ class SaleInvoice(db.Model):
     paymentStatus = db.Column(db.String(100))
     customer_id = Column(Integer, ForeignKey('user.id'), nullable=False)  # Khóa ngoại tới khách hàng
     sale_id = Column(Integer, ForeignKey('user.id'), nullable=True)  # Khóa ngoại tới nhân viên bán hàng
+    orderDate = db.Column(db.Date)
 
     def __str__(self):
         return self.name
@@ -122,8 +124,6 @@ class DetailInvoice(db.Model):
     id = db.Column(db.Integer, primary_key=True ,autoincrement=True)
     book_id = Column(Integer, ForeignKey(Book.id), nullable=False)
     saleInvoice_id = Column(Integer, ForeignKey(SaleInvoice.id), nullable=False)
-    orderDate = db.Column(db.Date)
-    totalAmount = db.Column(db.Float)
     quantity = db.Column(db.Integer)
 
     def __str__(self):
@@ -146,13 +146,24 @@ if __name__ == "__main__":
                 book = Book(**b)
                 db.session.add(book)
         db.session.commit()
-        new_user = User(
-            name='Nguyen Van A',
-            username='nva',
-            password='123',
-            email='customer@gmail.com',
-            address='123 Đường ABC, Thành phố XYZ',
-        )
+        new_user1 = User(name='Ho Duc Linh',username='HDL',password= str(hashlib.md5("hdl".encode('utf-8')).hexdigest()),email='hdl@gmail.com')
+        new_user2 = User(name='Nguyen Quang Khanh',username='NQK',password= str(hashlib.md5("nqk".encode('utf-8')).hexdigest()),email='nqk@gmail.com')
         # Thêm đối tượng vào cơ sở dữ liệu
-        db.session.add(new_user)
+        db.session.add_all([new_user1,new_user2])
+        db.session.commit()
+        sale_invoices = [
+            SaleInvoice(id=1, paymentStatus="Paid", customer_id=1, sale_id=None, orderDate=date(2024, 12, 1)),
+            SaleInvoice(id=2, paymentStatus="Pending", customer_id=1, sale_id=None, orderDate=date(2024, 12, 2)),
+            SaleInvoice(id=3, paymentStatus="Paid", customer_id=2, sale_id=None, orderDate=date(2024, 12, 3)),
+        ]
+
+        db.session.add_all(sale_invoices)
+        db.session.commit()
+        detail_invoices = [
+            DetailInvoice(id=1, book_id=1, saleInvoice_id=1, quantity=2),  # 2 quyển Python Basics
+            DetailInvoice(id=2, book_id=2, saleInvoice_id=1, quantity=1),  # 1 quyển Flask Web Development
+            DetailInvoice(id=3, book_id=3, saleInvoice_id=2, quantity=3),  # 3 quyển Database Design
+        ]
+
+        db.session.add_all(detail_invoices)
         db.session.commit()
