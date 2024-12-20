e0ace18949583ae1dc976264b0e5ada52dc02bc9
hoduclinh080204@gmail.com <hoduclinh080204@gmail.com>
2024-12-20 21:52:14 +0700

    update merge with hung2

--- bookstoremanagement/__init__.py
+++ bookstoremanagement/__init__.py
@@ -14,10 +14,10 @@ import cloudinary
 app = Flask(__name__)
 app.jinja_env.globals.update(int=int)
 
-app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:nguyenhung@localhost/bookstoredb?charset=utf8mb4"
+app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:08022004@localhost/bookstoredb?charset=utf8mb4"
 app.secret_key = 'ndjdjdkqjiqj@nsansjkaa'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-app.config["PAGE_SIZE"] = 4
+app.config["PAGE_SIZE"] = 16
 
 db = SQLAlchemy(app)
 login = LoginManager(app)
--- bookstoremanagement/models.py
+++ bookstoremanagement/models.py
@@ -159,41 +159,53 @@ class Regulation(db.Model):
 if __name__ == "__main__":
     with app.app_context():
         # db.create_all()
-        # stock = Stock(name = "Kho sach chinh")
+        # stock = Stock(name="Kho sach chinh")
         # db.session.add(stock)
         # db.session.commit()
-        # c1 = Category(name="Lap trinh", stock_id=1)
-        # c2 = Category(name="Ngon tinh", stock_id=1)
-        # c3 = Category(name="Thieu nhi", stock_id=1)
-        # db.session.add_all([c1, c2, c3])
+        #
+        # categories = [
+        #     "Lập trình", "Ngôn tình", "Kiếm hiệp", "Thiếu nhi", "Viễn tưởng",
+        #     "Khoa học", "Thời đại", "Trinh thám", "Cổ tích", "Giáo dục",
+        #     "Hoạt hình", "Công việc", "Kỹ năng sống", "Giao tiếp", "Ca nhạc kịch"
+        # ]
+        # # Tạo các đối tượng Category và thêm vào session
+        # category_objects = [Category(name=name, stock_id=1) for name in categories]
+        # db.session.add_all(category_objects)
         # db.session.commit()
+        #
         # with open('data/books.json', encoding='utf-8') as f:
         #     books = json.load(f)
         #     for b in books:
         #         book = Book(**b)
         #         db.session.add(book)
         # db.session.commit()
-        # new_user1 = User(name='Ho Duc Linh',username='HDL',password= str(hashlib.md5("hdl".encode('utf-8')).hexdigest()),email='hdl@gmail.com')
-        # new_user2 = User(name='Nguyen Quang Khanh',username='NQK',password= str(hashlib.md5("nqk".encode('utf-8')).hexdigest()),email='nqk@gmail.com')
-        # # Thêm đối tượng vào cơ sở dữ liệu
-        # db.session.add_all([new_user1,new_user2])
-        # db.session.commit()
-        # sale_invoices = [
-        #     SaleInvoice(id=1, paymentStatus="Paid",customer_name ="Ho Duc Linh", customer_id=1, sale_id=None, orderDate=date(2024, 12, 1)),
-        #     SaleInvoice(id=2, paymentStatus="Pending",customer_name ="Ho Duc Linh", customer_id=1, sale_id=None, orderDate=date(2024, 12, 2)),
-        #     SaleInvoice(id=3, paymentStatus="Paid",customer_name ="Nguyen Quang Khanh", customer_id=2, sale_id=None, orderDate=date(2024, 12, 3)),
-        # ]
         #
+        # new_user1 = User(name='Ho Duc Linh', username='HDL',
+        #                  password=str(hashlib.md5("hdl".encode('utf-8')).hexdigest()), email='hdl@gmail.com')
+        # new_user2 = User(name='Nguyen Quang Khanh', username='NQK',
+        #                  password=str(hashlib.md5("nqk".encode('utf-8')).hexdigest()), email='nqk@gmail.com')
+        # admin_user = User(name='Admin', username='admin', password=str(hashlib.md5('123'.encode('utf-8')).hexdigest()),
+        #                   user_role=UserRole.ADMIN)
+        # # Thêm đối tượng vào cơ sở dữ liệu
+        # db.session.add_all([new_user1, new_user2, admin_user])
+        # db.session.commit()
+        #
+        # sale_invoices = [
+        #     SaleInvoice(id=1, paymentStatus="Paid",customer_name="HDL", customer_id=1, sale_id=None, orderDate=date(2024, 12, 1)),
+        #     SaleInvoice(id=2, paymentStatus="Pending", customer_name="HDL",customer_id=1, sale_id=None, orderDate=date(2024, 12, 2)),
+        #     SaleInvoice(id=3, paymentStatus="Paid", customer_name="NQK",customer_id=2, sale_id=None, orderDate=date(2024, 12, 3)),
+        # ]
         # db.session.add_all(sale_invoices)
         # db.session.commit()
-        # detail_invoices = [
-        #     DetailInvoice(id=1, book_id=1, saleInvoice_id=1, quantity=2),
-        #     DetailInvoice(id=2, book_id=2, saleInvoice_id=1, quantity=1),
-        #     DetailInvoice(id=3, book_id=3, saleInvoice_id=2, quantity=3),
-        # ]
         #
+        # detail_invoices = [
+        #     DetailInvoice(id=1, book_id=1, saleInvoice_id=1, quantity=2),  # 2 quyển Python Basics
+        #     DetailInvoice(id=2, book_id=2, saleInvoice_id=1, quantity=1),  # 1 quyển Flask Web Development
+        #     DetailInvoice(id=3, book_id=3, saleInvoice_id=2, quantity=3),  # 3 quyển Database Design
+        # ]
         # db.session.add_all(detail_invoices)
         # db.session.commit()
+        #
         # favorites = [
         #     Favorite(book_id=1, customer_id=1),
         #     Favorite(book_id=8, customer_id=1),
@@ -202,11 +214,7 @@ if __name__ == "__main__":
         # ]
         # db.session.add_all(favorites)
         # db.session.commit()
-        # admin_user = User(name='Admin', username='admin', password=str(hashlib.md5('123'.encode('utf-8')).hexdigest()),
-        #                   user_role=UserRole.ADMIN)
-        new_user3 = User(name='Sale1', username='sale12',
-                         password=str(hashlib.md5("123".encode('utf-8')).hexdigest()), email='sale@gmail.com',
-                         user_role=UserRole.SALE)
-        db.session.add(new_user3)
+        sale = User(name='Sal12', username='Sale12', password=str(hashlib.md5('123'.encode('utf-8')).hexdigest()),
+                          user_role=UserRole.SALE)
+        db.session.add(sale)
         db.session.commit()
-
