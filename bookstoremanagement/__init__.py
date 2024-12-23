470efa6ebf31feadf7671a6f79dc7c13b0cf8b9d
Nguyen Hung <hungdao1481@gmail.com>
2024-12-24 01:47:02 +0700

    Reformat Price, adjusting Customer, Order n Flash

--- bookstoremanagement/__init__.py
+++ bookstoremanagement/__init__.py
@@ -14,7 +14,7 @@ import cloudinary
 app = Flask(__name__)
 app.jinja_env.globals.update(int=int)
 
-app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:08022004@localhost/bookstoredb?charset=utf8mb4"
+app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:nguyenhung@localhost/bookstoredb?charset=utf8mb4"
 app.secret_key = 'ndjdjdkqjiqj@nsansjkaa'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["PAGE_SIZE"] = 16
